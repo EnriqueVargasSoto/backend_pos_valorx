46d8b403455ce47d753d7bfd1f84f33588298236
EnriqueVargasSoto <45204929+EnriqueVargasSoto@users.noreply.github.com>
2023-11-21 18:47:29 -0500

    21/11/2023 18.47

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -49,4 +49,35 @@ class ProductController extends Controller
 
         return response()->json(['data' => $client]);
     }
+
+    public function saveSale(Request $request){
+        $urlValorx = 'http://www.valorx.net/Magicxpi4.12/MgWebRequester.dll?appname=IFSValorX&prgname=HTTP&arguments=-AHTTPVLXRest%23Sales&Compania='.$request->compania.'&Sucursal='.$request->sucursal;
+
+        $response = Http::post($urlValorx, $data = [
+            "plataforma_origen" => $request->plataforma_origen,//' : '1',
+            "usuario" => $request->usuario,//' : 'VENDEDOR1',
+            "cod_comprobante" => $request->cod_comprobante,//' : this.client.tipodocid == 'DNI' ? 'BOL' : 'FXV',
+            "serie_comprobante" => $request->serie_comprobante,//' : 'B801',
+            "fecha_comprobante" => $request->fecha_comprobante,//' : '2023-11-21',
+            "vendedor" => $request->vendedor,//' : '10247812',
+            "lista_precio" => $request->lista_precio,//' : environment.lista_precio,
+            "nro_document_ide" => $request->nro_document_ide,//' : this.client.nrodocide,
+            "client" => $request->client,//' : this.client.client,
+            "forma_pago" => $request->forma_pago,//' : "001",
+            "moneda" => $request->moneda,//' : 'PEN',
+            "detalle_sales" => $request->detalle_sales,//' : bodyDetail
+        ]);
+
+        //$data = iconv('ISO-8859-1', 'UTF-8', $response);
+        /*$response = str_replace("\n", "", $response);
+        $response = str_replace("\r", "", $response);
+        $response = str_replace("   ", "", $response);
+        $response = str_replace(" ", "", $response);*/
+        $response = str_replace(':,"', ':"', $response);
+        $response = str_replace('"cliente": ,', '"cliente": "",', $response);
+        //$data = iconv('ISO-8859-1', 'UTF-8', $response);
+        $sale = json_decode($response, true);
+
+        return response()->json($sale);
+    }
 }
